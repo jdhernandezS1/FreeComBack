8d82c3b32b7b374b1eca6681f8eda47840f465d7
David <julio-david@hotmail.com>
2024-02-20 22:45:04 +0100

    Add localhost

--- setup/settings.py
+++ setup/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "http://localhost:8000",
+]
 
 
 # Application definition
